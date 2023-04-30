83c2624f817d12adc42bd0564f7816fff29d6f70
fredjhosnick <fredjhosnickrios@gmail.com>
2023-04-30 16:50:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
+
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+
+
 function App() {
+  const [movieName, setMovieName] = useState('');
+  const [container, setContainer] = useState([]);
+  const[foundMovie, setFoundMovie] = useState('');
+
+
+  useEffect(() => {
+   fetchMe()
+  },[foundMovie]) 
+
+ const fetchMe =()=>{
+
+
+  fetch(`https://imdb8.p.rapidapi.com/auto-complete?q=+${movieName}`,{
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5100c24fcfmshe3c6b7ee56a3069p164f69jsnc2db4c04f16f',
+		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+	}
+})
+
+.then(response =>{
+  return response.json()
+})
+.then(data =>{
+  setContainer(data.d)
+})
+.catch(err =>{
+  console.error(err);
+});
+
+ }
+
+const onChangeHandler =(e)=>{
+  setMovieName(e.target.value);
+
+}
+const submitHandler= (e) =>{
+  e.preventDefault();
+  setFoundMovie(movieName);
+
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitHandler}> 
+        <input 
+        placeholder='search the movie'
+        type='text' 
+        value={movieName} onChange={onChangeHandler}/>
+        <button type='submit'>Search</button>
+      </form>
+
+      {container.length > 0  && (<div className='element'>
+      {container.map((item , index) =>{
+
+           return (
+            <div key={index} className='element-div'>
+            <img src={item.i.imageUrl}/>
+            <p>{item.l}</p>
+            <p>{item.s}</p>
+            </div>
+           )
+      })}
+     
+    </div>)}
+   
     </div>
   );
 }
